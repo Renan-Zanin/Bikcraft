72ff94566bc94f579839b8980ca187cb866707dd
Renan-Zanin <82837063+Renan-Zanin@users.noreply.github.com>
2021-05-07 19:08:43 -0300

    Add files via upload

---
+++ enviar-sendgrid.php
@@ -0,0 +1,36 @@
+<?php
+
+require("./sendgrid-php/sendgrid-php.php");
+
+$email_site = "renan.oliveira.ismart@gmail.com";
+$nome_site = "Bikcraft";
+
+$email_user = $_POST["email"];
+$nome_user = $_POST["nome"];
+
+$body_content = "";
+foreach( $_POST as $field => $value) {
+  if( $field !== "leaveblank" && $field !== "dontchange" && $field !== "enviar") {
+    $sanitize_value = filter_var($value, FILTER_SANITIZE_STRING);
+    $body_content .= "$field: $value \n";
+  }
+}
+
+$email = new \SendGrid\Mail\Mail(); 
+$email->setFrom($email_site, $nome_site);
+$email->addTo($email_site, $nome_site);
+
+$email->setReplyTo($email_user, $nome_user);
+
+$email->setSubject("Formulário Bikcraft");
+$email->addContent("text/plain", $body_content);
+
+$sendgrid = new \SendGrid("dbad82335ffa827a8fc3c8683184d2b1-2a9a428a-0f3bf262");
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo "Caught exception: ". $e->getMessage() ."\n";
+}
